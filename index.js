5b46571197b61af5e232f7e6548448b2622c5642
netprtony <huynhvikhang913@gmail.com>
2022-07-02 09:34:10 +0700

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-function checkForm(){
-    const user = document.getElementsById('user').value;
-    document.getElementById('results').innerText = user;
-    console.log(user);
+let countEl = document.getElementById("count-el")
+let count = 0 
+function incerement(){
+    count = count + 1
+    countEl.innerText= count
 }
